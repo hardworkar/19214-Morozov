53a46989de34fde31f4b85094280bd74c20a345b
hardworkar <52618858+hardworkar@users.noreply.github.com>
2019-09-21 00:06:23 +0700

    Add files via upload

--- abc.hs
+++ abc.hs
@@ -1,7 +1,10 @@
-d a b c = sqrt(b*b-4*a*c)
-x1 a b c = abs0((-b + d a b c)/2/a)
-x2 a b c = abs0((-b - d a b c)/2/a)
+d a b c = b*b-4*a*c
 abs0 x | x == -0 = 0
        | True = x
-solve a b c | d a b c >= 0 = (x1 a b c, x2 a b c)
+x1 a b c = abs0((-b + sqrt(d a b c))/2/a)
+x2 a b c = abs0((-b - sqrt(d a b c))/2/a)
+solve a b c | d a b c >= 0 && a /= 0 = (x1 a b c, x2 a b c)
+            | a == 0 && b /= 0 = (-c/b,-c/b)
+            | a == 0 && b == 0 && c == 0 = error "Infinity"
+            | a == 0 && b == 0 && c /= 0 = error "No solutions"
             | True = error "No real solutions"
