c798266d35b4e5d50eb7e7282bc931934759ea59
hardworkar <52618858+hardworkar@users.noreply.github.com>
2019-10-01 05:25:30 +0300

    Neg disc
    Reworking negative discriminant

--- abc.hs
+++ abc.hs
@@ -1,6 +1,6 @@
-solve 0 0 0 = error "infinity"
+solve 0 0 0 = error "Infinity"
 solve 0 0 c = error "No solutions"
 solve 0 b c = (-c/b,-c/b)
-solve a b c = (x1,x2) where d = b*b - 4*a*c 
-                            x1 = ((- b + sqrt (d)) / 2 / a)
-                            x2 = ((- b - sqrt (d)) / 2 / a)
+solve a b c | d >= 0 = (((- b - sqrt (d)) / 2 / a),((- b + sqrt (d)) / 2 / a))
+            | otherwise = error "Negative discriminant"
+            where d = b*b - 4*a*c
