b7a0e068790c65a7bd74e371f479eb89bbb635d2
hardworkar <52618858+hardworkar@users.noreply.github.com>
2019-11-04 19:17:45 +0700

    Add files via upload
    Исправлено:
    -- гварды
    -- foldl toDecimal --> разбор через степени 10
    -- разобрать проверку через ['a'..'z']

--- lab2.hs
+++ lab2.hs
@@ -1,25 +1,27 @@
 import Data.Char
 import Data.List
-toDecimal:: Int -> String -> String
+toDecimal :: Int -> String -> String
 toDecimal base [] = "0"
-toDecimal 1 snumber = if null((filter (/='1') snumber)) then show(length snumber - 1) else error "that's illegal"
-toDecimal base snumber = helper base snumber 0 where
-                         helper base [] acc = show acc
-                         helper base (x:snumber) acc = helper base snumber (acc + (digitToInt'(x) base)*base^length(snumber))
-                         digitToInt' a base = (if ((ord a > 47) && (ord a < 58) && (ord a - 48 < base)) then (ord a - 48) else (if ((ord a > 64) && (ord a < 91) && (ord a - 29 < base)) then (ord a - 29) else (if ((ord a > 96) && (ord a < 123) && (ord a - 87 < base)) then (ord a - 87) else error "digit is not ok")))
-fromDecimal:: Int -> String -> String
-fromDecimal 1 snumber = helper [] (wordToInt(snumber)) where
-                        helper acc 0 = '1':acc
-                        helper acc n = helper ('1':acc) (n-1)
-                        wordToInt [] = 0
-                        wordToInt (x:word) = (digitToInt'(x)*10^length(word) + wordToInt word)
-                        digitToInt' a = (if ((ord a > 47) && (ord a < 58) && (ord a - 48 < 10)) then (ord a - 48) else error "digit is not ok")
-fromDecimal toBase snumber = helper toBase (wordToInt(snumber)) [] where
+toDecimal 1 snumber = if all (=='1') snumber then show(length snumber - 1) else error "wait, that's illegal"
+
+toDecimal base snumber = show (foldl (\acc x -> acc*base + (digitToInt'(x) base)) 0 snumber) where
+                              digitToInt' a base | ((elem a ['0'..'9']) && (ord a - ord '0' + 0 < base)) = ord a - ord '0' + 0
+                                                 | ((elem a ['a'..'z']) && (ord a - ord 'a' + 10 < base)) = ord a - ord 'a' + 10
+                                                 | ((elem a ['A'..'Z']) && (ord a - ord 'A' + 36 < base)) = ord a - ord 'A' + 36
+                                                 | otherwise = error "digit is not ok"
+
+fromDecimal :: Int -> String -> String
+fromDecimal 1 snumber = replicate (read(snumber)+1) '1'
+fromDecimal toBase snumber = helper toBase (read(snumber)) [] where
                              helper toBase 0 [] = "0"
                              helper toBase 0 acc = acc
-                             helper toBase number acc = helper toBase (div number toBase) ((chr ((if (mod number toBase) < 10 then (mod number toBase+48) else (if ((mod number toBase) > 9 && (mod number toBase) < 36) then (mod number toBase+87) else (if ((mod number toBase) > 35 && (mod number toBase) < 62) then (mod number toBase+29) else error "digit is not ok")))):acc))
-                             wordToInt [] = 0
-                             wordToInt (x:word) = (digitToInt'(x)*10^length(word) + wordToInt word)
-                             digitToInt' a = (if ((ord a > 47) && (ord a < 58) && (ord a - 48 < 10)) then (ord a - 48) else error "digit is not ok")
-convertFromTo::Int -> Int -> String -> String
+                             helper toBase number acc = helper toBase (div number toBase) (chr(numToChr number toBase):acc)
+                             numToChr a toBase | (elem (mod a toBase) [0..9]) = (mod a toBase + ord '0' - 0)
+                                               | (elem (mod a toBase) [10..35]) = (mod a toBase + ord 'a' - 10)
+                                               | (elem (mod a toBase) [36..61]) = (mod a toBase + ord 'A' - 36)
+                                               | otherwise = error "digit is not ok"
+convertFromTo :: Int -> Int -> String -> String
 convertFromTo fromBase toBase snumber = fromDecimal toBase (toDecimal fromBase snumber)
+-- гварды
+-- foldl toDecimal --> разбор через степени 10
+-- разобрать проверку через ['a'..'z']
