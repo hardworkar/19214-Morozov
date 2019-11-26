815951156e9f80fa99fc80fb257d0dbc79b162f6
hardworkar <52618858+hardworkar@users.noreply.github.com>
2019-11-26 17:02:26 +0700

    ++ scalar & entagle (насколько я понял что это)

--- quantumstates.hs
+++ quantumstates.hs
@@ -7,43 +7,49 @@ instance (Show a) => Show (Complex a) where
    show (Complex r i) = show r ++ "+" ++ "(" ++ show i ++ "i)"
 
 
-x = Complex 0 0
-y = Complex 12 32
+x = (Complex 4 7)
+y = (Complex 1 3)
 
 data QuantumState a = QuantumState a String
+
 instance Functor QuantumState where
    fmap f (QuantumState comp str) = QuantumState (f comp) str
 
 instance (Eq a) => Eq (QuantumState a) where
    (==) (QuantumState c1 str1) (QuantumState c2 str2) = c1==c2 && str1==str2
 instance (Show a) => Show (QuantumState a) where
-   show (QuantumState c s) = show c ++ " metka: " ++ s
+   show (QuantumState c s) = show c ++ " note: " ++ s
 
 type Qubit a = [QuantumState a]
 
-toList::Qubit a -> [a]
+toList :: Qubit (Complex a) -> [Complex a]
 toList [] = []
 toList ((QuantumState comp str):xs) = comp:(toList xs)
 
-toLabelList::Qubit a -> [String]
+toLabelList :: Qubit (Complex a) -> [String]
 toLabelList [] = []
 toLabelList ((QuantumState comp str):xs) = str:(toLabelList xs)
 
-fromList:: [a]->[String]->Qubit a
+fromList :: [Complex a]->[String]->Qubit (Complex a)
 fromList [] [] = []
 fromList (c:cmps) (s:strs) = (QuantumState c s):fromList cmps strs
 
-toPairList:: Qubit a->[(a,String)]
+toPairList :: Qubit (Complex a) -> [(Complex a,String)]
 toPairList [] = []
 toPairList ((QuantumState comp str):xs) = (comp,str):(toPairList xs)
 
-fromPairList:: [(a,String)] -> Qubit a
+fromPairList :: [(Complex a,String)] -> Qubit (Complex a)
 fromPairList [] = []
 fromPairList ((c,s):xs) = (QuantumState c s):fromPairList xs
 
+scalarProduct:: (Num a) => Qubit (Complex a) -> Qubit (Complex a) -> Complex a
+scalarProduct qub1 qub2 = foldl1 (\(Complex rAcc iAcc) (Complex rCurr iCurr)-> Complex (rAcc+rCurr) (iAcc+iCurr)) (zipWith (\(Complex r1 i1) (Complex r2 i2) -> (Complex (r1*r2-i1*i2) (i1*r2+r1*i2))) (toList qub1) (toList qub2))
+
+entagle::(Num a) => Qubit (Complex a) ->Qubit (Complex a) ->Qubit (Complex a)
+entagle qub1 qub2 = [QuantumState (Complex (r1*r2-i1*i2) (i1*r2+r1*i2)) (s1++s2) | (QuantumState (Complex r1 i1) s1) <- qub1,(QuantumState (Complex r2 i2) s2) <- qub2]
 
 
-qub = [x,x,y]
+qub = [q,w]
 
 q = QuantumState x "uuu"
 w = QuantumState y "www"
