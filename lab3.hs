776b27f7fc4ddbc7331bc4a08730237799c69f91
hardworkar <52618858+hardworkar@users.noreply.github.com>
2019-12-21 01:47:42 +0700

    вторая версия хеш-таблицы

--- lab3.hs
+++ lab3.hs
@@ -9,9 +9,9 @@ main::IO()
 main = do
       contents <- lines <$> readFile "result.txt"
       let pairList = [(head $ words x , last $ words x) | x <- contents]
-      let table = (fromList pairList 50)
+      let table = (fromList pairList)
       print (erase table "0")
-      print (at table "50")
+      print (at table "9999")
       print (contains table "100")
 
 
@@ -21,17 +21,18 @@ defaultHashTable = HashTable (replicate 10 []) 10 0
 hash :: (Show k) => k -> Int -> Int
 hash k size = (foldl (\acc x -> acc + ord(x)) 0 (show k)) `mod` size
 
-fromList :: (Show k, Eq k)=> [(k,v)]-> Size -> HashTable k v
-fromList xs size = foldl (\acc (a,b) -> (insert' acc a b)) (HashTable (replicate size []) size 0) xs
+fromList :: (Show k, Eq k)=> [(k,v)]-> HashTable k v
+fromList xs = foldl (\acc (a,b) -> (insert' acc a b)) (HashTable (replicate len []) len 0) xs where
+                                                                                                  len = (length xs)
 
 
 clear :: HashTable k v -> HashTable k v
 clear table = defaultHashTable 
 
 insert' ::(Show k, Eq k)=>HashTable k v ->k->v->HashTable k v
-insert' (HashTable table size occupied) k v = if (2*occupied < size) then (HashTable (left ++ mid ++ (drop 1 right)) size newSize) 
+insert' (HashTable table size occupied) k v = if (2*occupied < size) then (HashTable (left ++ mid ++ (drop 1 right)) size newOccupied) 
                                               else insert' (rehash table size) k v where
-                                                                        newSize = occupied + 1
+                                                                        newOccupied = occupied + 1
                                                                         mid | elem k [key | (key,v) <- current] = [current]
                                                                             | otherwise = [(k,v):current]
                                                                         splited = splitAt (hash k size) table
@@ -40,7 +41,10 @@ insert' (HashTable table size occupied) k v = if (2*occupied < size) then (HashT
                                                                         right = snd(splited)
 
 rehash :: (Show k, Eq k)=>[[(k,v)]] -> Size -> HashTable k v
-rehash table size = fromList [x | x <- (foldl (++) [] table)] (size*2)
+rehash table size = foldl (\acc (a,b) -> (insert' acc a b)) newBox allPairs where
+                                                                                newSize = 2*size
+                                                                                newBox = (HashTable (replicate newSize []) newSize 0)
+                                                                                allPairs = [x | x <- (foldl (++) [] table)]
 
 erase::(Show k, Eq k)=>HashTable k v->k->HashTable k v
 erase (HashTable table size occupied) k = (HashTable (left ++ mid ++ (drop 1 right)) size newSize) where
@@ -53,14 +57,13 @@ erase (HashTable table size occupied) k = (HashTable (left ++ mid ++ (drop 1 rig
 
 contains::(Show k, Eq k)=>HashTable k v -> k -> Bool
 contains (HashTable table size occupied) k = elem k [key | (key,v) <- current] where
-                                                                        splited = splitAt (hash k size) table
-                                                                        current = head(right)
-                                                                        right = snd(splited)
+                                                            right = drop (hash k size) table
+                                                            current = head(right)
 
 at::(Show k, Eq k)=>HashTable k v -> k -> Maybe v
 at (HashTable table size occupied) k =  lookup k current where
-                                                            splited = splitAt (hash k size) table
-                                                            current = head(snd(splited))
+                                                            right = drop (hash k size) table
+                                                            current = head(right)
 
 size::(Show k, Eq k)=>HashTable k v -> Integer
 size (HashTable table size occupied) = toInteger(occupied)
