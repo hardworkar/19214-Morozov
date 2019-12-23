c891c84f6d1e631efa76e523583e41a0d2794e84
hardworkar <52618858+hardworkar@users.noreply.github.com>
2019-12-23 20:06:06 +0700

    правки с пары
    at+contain в одну строчку, capacity <-> size, concat [[]], паттерн в splited, перезапись по ключу

--- lab3.hs
+++ lab3.hs
@@ -1,9 +1,9 @@
 import System.IO
 import Data.Char
 import Data.List
+type Capacity = Int
 type Size = Int
-type Occupied = Int
-data HashTable k v = HashTable [[(k,v)]] Size Occupied deriving (Show)
+data HashTable k v = HashTable [[(k,v)]] Capacity Size deriving (Show)
 
 main::IO()
 main = do
@@ -19,7 +19,7 @@ defaultHashTable :: HashTable k v
 defaultHashTable = HashTable (replicate 10 []) 10 0
 
 hash :: (Show k) => k -> Int -> Int
-hash k size = (foldl (\acc x -> acc + ord(x)) 0 (show k)) `mod` size
+hash k cap = (foldl (\acc x -> acc + ord(x)) 0 (show k)) `mod` cap
 
 fromList :: (Show k, Eq k)=> [(k,v)]-> HashTable k v
 fromList xs = foldl (\acc (a,b) -> (insert' acc a b)) (HashTable (replicate len []) len 0) xs where
@@ -30,45 +30,32 @@ clear :: HashTable k v -> HashTable k v
 clear table = defaultHashTable 
 
 insert' ::(Show k, Eq k)=>HashTable k v ->k->v->HashTable k v
-insert' (HashTable table size occupied) k v = if (2*occupied < size) then (HashTable (left ++ mid ++ (drop 1 right)) size newOccupied) 
-                                              else insert' (rehash table size) k v where
-                                                                        newOccupied = occupied + 1
-                                                                        mid | elem k [key | (key,v) <- current] = [current]
-                                                                            | otherwise = [(k,v):current]
-                                                                        splited = splitAt (hash k size) table
-                                                                        left = fst(splited)
-                                                                        current = head(right)
-                                                                        right = snd(splited)
+insert' (HashTable table cap size) k v = if (2*size < cap) then (HashTable (left ++ mid ++ (drop 1 right)) cap newSize) 
+                                              else insert' (rehash table cap) k v where
+                                                                        newSize = size + 1
+                                                                        mid = [(k,v):(filter (\x -> (fst x) /= k) current)]
+                                                                        current = head right
+                                                                        (left,right) = splitAt (hash k cap) table
 
-rehash :: (Show k, Eq k)=>[[(k,v)]] -> Size -> HashTable k v
-rehash table size = foldl (\acc (a,b) -> (insert' acc a b)) newBox allPairs where
-                                                                                newSize = 2*size
-                                                                                newBox = (HashTable (replicate newSize []) newSize 0)
-                                                                                allPairs = [x | x <- (foldl (++) [] table)]
+rehash :: (Show k, Eq k)=>[[(k,v)]] -> Capacity -> HashTable k v
+rehash table cap = foldl (\acc (a,b) -> (insert' acc a b)) newBox (concat table) where
+                                                                                newCap = 2*cap
+                                                                                newBox = (HashTable (replicate newCap []) newCap 0)
 
 erase::(Show k, Eq k)=>HashTable k v->k->HashTable k v
-erase (HashTable table size occupied) k = (HashTable (left ++ mid ++ (drop 1 right)) size newSize) where
-                                                                        newSize = occupied - 1
-                                                                        mid = [filter (\x -> (fst x) /= k) current]
-                                                                        splited = splitAt (hash k size) table
-                                                                        left = fst(splited)
-                                                                        current = head(right)
-                                                                        right = snd(splited)
+erase (HashTable table cap size) k = (HashTable (left ++ mid ++ (drop 1 right)) cap newSize) where
+                                                                        newSize = size - 1
+                                                                        mid = [filter (\x -> (fst x) /= k) (head right)]
+                                                                        (left,right) = splitAt (hash k cap) table
 
 contains::(Show k, Eq k)=>HashTable k v -> k -> Bool
-contains (HashTable table size occupied) k = elem k [key | (key,v) <- current] where
-                                                            right = drop (hash k size) table
-                                                            current = head(right)
+contains (HashTable table cap _) k = elem k [key | (key,v) <- table !! (hash k cap)]
 
 at::(Show k, Eq k)=>HashTable k v -> k -> Maybe v
-at (HashTable table size occupied) k =  lookup k current where
-                                                            right = drop (hash k size) table
-                                                            current = head(right)
+at (HashTable table cap _) k =  lookup k (table !! (hash k cap))
 
 size::(Show k, Eq k)=>HashTable k v -> Integer
-size (HashTable table size occupied) = toInteger(occupied)
+size (HashTable _ _ size) = toInteger(size)
 
 empty::(Show k, Eq k)=>HashTable k v -> Bool
-empty (HashTable table size occupied) = occupied == 0
-
-x = defaultHashTable
+empty (HashTable _ _ size) = size == 0
